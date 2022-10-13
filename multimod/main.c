e8565987cda174a961d7b0eaa538b271e0524089
tracer-nju <tracer@nju.edu.cn>
2022-10-13 22:52:16 +0800

    Static hostname: yunju-Lenovo-ThinkBook-16p-Gen-2        Icon name: computer-laptop          Chassis: laptop       Machine ID: b6c6b54e01274fa7a974ccc50b5b9f34          Boot ID: 80c54290c0cf4d76ac472cd60f1ff108 Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-48-generic     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo ThinkBook 16p Gen 2  22:52:16 up  1:08,  1 user,  load average: 0.66, 0.90, 0.84

--- multimod/main.c
+++ multimod/main.c
@@ -1,47 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <inttypes.h>
-static inline int num_of_digit(unsigned long x)
-{
-  int num=0;
-  x=x>>1;
-  while(x)
-  {x=x>>1;
-    num++;
-  }
-  return num;
 
-}
-uint64_t mod(uint64_t a,uint64_t b)
-{
-  if(b>a)
-  return a;
-  else
-  {
-    int adigit=num_of_digit(a);
-    int bdigit=num_of_digit(b);
-    if(adigit==bdigit)
-    return a-b;
-    else
-    // return mod(sub(a,b<<(sub(adigit,add(bdigit,1)))),b);
-    return mod(a-(b<<(adigit-bdigit-1)),b);
-  }
-}
-uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-int i=0;
-uint64_t a1=a>>1;
-uint64_t sum=0;
-while(a)
-{
-if(a^(a1<<1))
-sum+=(b<<i);
-a=a1;
-a1=a1>>1;
-i++;
-
-}
-  return mod(sum,m); 
-}
+uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) 
 
 void test(uint64_t a, uint64_t b, uint64_t m) {
   #define U64 "%" PRIu64
